e0e4df9934f2602f2e6e49cc9ed07253dbeb1015
Vlad Test <vladkretovsky@yandex.ru>
2024-06-28 15:54:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int ticet_cost = 35839;
+        int miles = ticet_cost / 20;
+
+        System.out.println(miles);
+
+    }
+}
